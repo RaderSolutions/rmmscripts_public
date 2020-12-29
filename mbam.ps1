33c59e94490548bfe6458f23bdaebfdc03b09788
tfournet <tfournet@radersolutions.com>
2020-12-29 13:27:31 -0600

    Create mbam.ps1

---
+++ mbam.ps1
@@ -0,0 +1,12 @@
+$key=$args[0]
+
+downloadUrl="https://toolset.malwarebytes.com/file/mbts/$key"
+
+tempDir="C:\Windows\Temp"
+dlPath="$tempDir\mbts.zip"
+
+(New-Object System.Net.WebClient).DownloadFile($downloadUrl, $dlPath)  
+
+Expand-Archive -Path $destPth -DestinationPath $tempDir
+
+Start-Process -FilePath $tempDir\
