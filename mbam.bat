e35b49fb6b41b4a8212a74d9e8152fdb9863ca2e
tfournet <tfournet@radersolutions.com>
2020-12-29 13:35:12 -0600

    Update mbam.bat

--- mbam.bat
+++ mbam.bat
@@ -1,1 +1,3 @@
-powershell -c "Invoke-WebRequest -Uri 'https://www.website.com/file.zip' -OutFile '%TEMP%\mbam.ps1"
+
+powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/RaderSolutions/rmmscripts_public/master/mbam.ps1' -OutFile '%TEMP%\mbam.ps1'"
+powershell -c "Start-Process $env:TEMP\mbam.ps1 -ArgumentList CB9YM-XY6ZB-X43YF-X2UVX"
