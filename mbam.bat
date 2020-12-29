2a8f151913b4764d96844d5dd249e718757ac42c
tfournet <tfournet@radersolutions.com>
2020-12-29 13:38:17 -0600

    Delete mbam.bat

--- mbam.bat
+++
@@ -1,3 +0,0 @@
-
-powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/RaderSolutions/rmmscripts_public/master/mbam.ps1' -OutFile '%TEMP%\mbam.ps1'"
-powershell -c "Start-Process $env:TEMP\mbam.ps1 -ArgumentList CB9YM-XY6ZB-X43YF-X2UVX"
