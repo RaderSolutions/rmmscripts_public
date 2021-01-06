738d9597801a968bab19d312d61b6546c8e633f5
tfournet <tfournet@radersolutions.com>
2021-01-06 12:37:09 -0600

    Delete Enable-StrongCrypto.ps1

--- Security/Perch/perch-auditbeats-windowsShares/pwsh/Enable-StrongCrypto.ps1
+++
@@ -1,3 +0,0 @@
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
-[Net.ServicePointManager]::SecurityProtocol
